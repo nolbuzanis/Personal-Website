73ff5a98878ac65a7b5972a91dccfe430eb7568b
nolbuzanis <nolanbuzanis@gmail.com>
2019-02-24 19:23:29 -0500

    Created/ styled contactlist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ContactList from './ContactList';
 
+import './App.css';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <ContactList />
+      <div className='app'>
+        <div className='header'>
+          <ContactList />
+        </div>
       </div>
     );
   }
--- src/components/ContactIcon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ContactIcon = () => {
-  return <div>Icon</div>;
-};
-
-export default ContactIcon;
