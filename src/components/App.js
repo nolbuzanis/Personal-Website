2b7de196a69f80f7936c67d878bb47b0a7c2c857
nolbuzanis <nolanbuzanis@gmail.com>
2019-03-02 16:29:52 -0500

    Created optimized gh-pages build

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ContactList from './ContactList';
 import PortfolioGrid from './PortfolioGrid';
 import Modal from './Modal';
+import Particles from 'react-particles-js';
 //import AboutSection from './AboutSection';
 
 import './App.css';
@@ -21,6 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='app'>
+        <Particles className='particles' params={particleParams} />
         <div className='header'>
           <div className='header-content'>
             <h1>Nolan Buzanis</h1>
@@ -76,8 +78,8 @@ const projects = [
     img: 'bitsintobites.png',
     content:
       'React-based web app to use up leftover food in the fridge at the end of the week. Search terms are run through the Edamam API, and the results are displayed on the page, where each recipe has an image, calorie count, ingrdient list, health labels, and a link to the page the recipe was originally sourced from.',
-    github: null,
-    link: null
+    github: 'https://github.com/nolbuzanis/Bits-Into-Bites',
+    link: 'https://nolbuzanis.github.io/Bits-Into-Bites/'
   },
   {
     title: 'Quadcopter',
@@ -112,5 +114,122 @@ const projects = [
     link: null
   }
 ];
+
+const particleParams = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#fcf3e2'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.3,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: false,
+        mode: 'grab'
+      },
+      onclick: {
+        enable: false,
+        mode: 'push'
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 200,
+        line_linked: {
+          opacity: 0.5
+        }
+      },
+      bubble: {
+        distance: 150,
+        size: 15,
+        duration: 2,
+        opacity: 1,
+        speed: 3
+      },
+      repulse: {
+        distance: 200
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true,
+  config_demo: {
+    hide_card: false,
+    background_color: '#b61924',
+    background_image: '',
+    background_position: '50% 50%',
+    background_repeat: 'no-repeat',
+    background_size: 'cover'
+  }
+};
 
 export default App;
